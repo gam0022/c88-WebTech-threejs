1c72ed2c52df144b7a1c8484a8ea9d632164d859
gam0022 <gam0022@gmail.com>
2015-07-20 00:28:46 +0900

    追加 - TypeScript によるサンプル - Phongシェーディング - ウィンドウに追従したリサイズ

---
+++ examples/hello.js
@@ -0,0 +1,36 @@
+/**
+ *
+ * Created by gam0022 on 15/07/19.
+ */
+/// <reference path="../typings/bundle.d.ts"/>
+var Hello = (function () {
+    function Hello() {
+        var _this = this;
+        this.animate = function () {
+            requestAnimationFrame(_this.animate);
+            _this.mesh.rotation.x += 0.01;
+            _this.mesh.rotation.y += 0.02;
+            _this.renderer.render(_this.scene, _this.camera);
+        };
+        this.init();
+        this.animate();
+    }
+    Hello.prototype.init = function () {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        this.camera.position.z = 1000;
+        var geometry = new THREE.BoxGeometry(400, 400, 400);
+        var material = new THREE.MeshBasicMaterial({
+            color: 0xff0000,
+            wireframe: true
+        });
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.scene.add(this.mesh);
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setClearColor(new THREE.Color(0xffffff));
+        document.body.appendChild(this.renderer.domElement);
+    };
+    return Hello;
+})();
+//# sourceMappingURL=hello.js.map
---
+++ examples/hello.ts
@@ -0,0 +1,52 @@
+/**
+ *
+ * Created by gam0022 on 15/07/19.
+ */
+
+
+/// <reference path="../typings/bundle.d.ts"/>
+
+class Hello {
+  scene:THREE.Scene;
+  camera:THREE.Camera;
+  renderer:THREE.Renderer;
+  mesh:THREE.Mesh;
+
+  constructor() {
+    this.init();
+    this.animate();
+  }
+
+  init() {
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(75,
+      window.innerWidth / window.innerHeight, 1, 10000);
+    this.camera.position.z = 1000;
+
+    var geometry = new THREE.BoxGeometry(400, 400, 400);
+    var material = new THREE.MeshBasicMaterial({
+      color: 0xff0000,
+      wireframe: true
+    });
+
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(this.mesh);
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setClearColor(new THREE.Color(0xffffff));
+
+    document.body.appendChild(this.renderer.domElement);
+  }
+
+  animate = () => {
+
+    requestAnimationFrame(this.animate);
+
+    this.mesh.rotation.x += 0.01;
+    this.mesh.rotation.y += 0.02;
+
+    this.renderer.render(this.scene, this.camera);
+  }
+}
